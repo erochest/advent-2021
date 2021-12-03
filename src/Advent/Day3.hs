731f41713241159a5ed52ce7ae12d81e61ea36d9
Eric Rochester <erochest@gmail.com>
2021-12-03 06:31:38 -0500

    Stub in day 3

---
+++ src/Advent/Day3.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Advent.Day3 
+  ( partOne
+  -- , partTwo
+  ) where
+
+import Advent
+
+partOne :: [()] -> Int
+partOne = undefined
+
+instance Parseable () where
+  parse _ = Nothing
