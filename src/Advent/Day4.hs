a880db44d6e81f662afd139a032a8f4acd7cdb97
Eric Rochester <erochest@gmail.com>
2021-12-03 20:53:09 -0500

    stubbed in day 4

---
+++ src/Advent/Day4.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Advent.Day4
+  ( partOne
+  , partTwo
+  ) where
+
+import Advent
+
+partOne :: [()] -> Int
+partOne = undefined
+
+partTwo :: [()] -> Int
+partTwo = undefined
+
+instance Parseable () where
+  parse _ = Nothing
---
+++ test/day4/Spec.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Advent
+import Advent.Day4
+
+main :: IO ()
+main = defaultMain suite
+
+suite :: TestTree
+suite = testGroup "day4"
+    [
+    ]
+
