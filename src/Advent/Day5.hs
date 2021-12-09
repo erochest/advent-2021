baf743162e7a5b4472211d764fef677cfb2d2d12
Eric Rochester <erochest@gmail.com>
2021-12-09 05:50:03 -0500

    day 5, part 1

--- src/Advent/Day5.hs
+++ src/Advent/Day5.hs
@@ -2,21 +2,80 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
 
 module Advent.Day5
   ( partOne
   , partTwo
+  , mapDimension
+  , Point
+  , Segment
+  , Map
+  , createMap
+  , mapSegments
+  , expandSegment
+  , pointIndex
+  , countDoubled
   ) where
+
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as M
 
 import Debug.Trace
 
 import Advent
 
-partOne :: () -> Int
-partOne = undefined
+partOne :: [Segment] -> Int
+partOne = countDoubled . mapSegments createMap
 
-partTwo :: () -> Int
+partTwo :: [Segment] -> Int
 partTwo = undefined
+
+mapDimension :: Int
+mapDimension = 1000
+
+type Point = (Int, Int)
+type Segment = (Point, Point)
+type Map = V.Vector Int
+
+instance Parseable Segment where
+  parse = either (const Nothing) Just . parseOnly segment . C.toStrict
+
+segment :: Parser Segment
+segment = (,) <$> point <*> (" -> " *> point)
+
+point :: Parser Point
+point = (,) <$> decimal <*> (char8 ',' *> decimal)
+
+createMap :: Map
+createMap = V.replicate (mapDimension * mapDimension) 0
+
+mapSegments :: Map -> [Segment] -> Map
+mapSegments m segments = V.accum (+) m $ map ((, 1) . pointIndex) (concatMap expandSegment segments)
+
+expandSegment :: Segment -> [Point]
+expandSegment (start@(x0, y0), end@(x1, y1))
+  | x0 == x1 || y0 == y1 = expand (offset x0 x1, offset y0 y1) start end
+  | otherwise            = []
+  where
+    offset a b = case compare a b of
+                  LT -> 1
+                  EQ -> 0
+                  GT -> (-1)
+    expand offset@(offsetX, offsetY) start@(x0, y0) end
+      | start == end = [end]
+      | otherwise    = let next = (x0 + offsetX, y0 + offsetY)
+                       in  start:expand offset next end
+
+pointIndex :: Point -> Int
+pointIndex (x, y) = x + y * mapDimension
+
+countDoubled :: Map -> Int
+countDoubled = V.length . V.filter (>1)
 
 instance Parseable () where
   parse _ = Nothing
--- test/day5/Spec.hs
+++ test/day5/Spec.hs
@@ -13,18 +13,103 @@ import Test.Tasty.HUnit
 import Advent
 import Advent.Day5
 
+import Debug.Trace
+
 main :: IO ()
 main = defaultMain suite
 
-fixture :: ()
-fixture = ()
+fixture :: [Segment]
+fixture = [ ((0,9), (5,9))
+          , ((8,0), (0,8))
+          , ((9,4), (3,4))
+          , ((2,2), (2,1))
+          , ((7,0), (7,4))
+          , ((6,4), (2,0))
+          , ((0,9), (2,9))
+          , ((3,4), (1,4))
+          , ((0,0), (8,8))
+          , ((5,5), (8,2))
+          ]
 
 suite :: TestTree
 suite = testGroup "day 5"
     [ testGroup "part 1"
       [ testGroup "partOne"
         [ testCase "finds the answer" $
-          partOne fixture @?= 0
+          partOne fixture @?= 5
+        ]
+      , testGroup "parse"
+        [ testCase "parses an input line" $ do
+          let expected :: Segment = ((0, 9), (5, 9))
+          parse "0,9 -> 5,9" @?= Just expected
+        ]
+      , testGroup "countDoubled"
+        [ testCase "counts the number of items greater than 1" $ do
+          let input = [ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+                      , 0, 0, 1, 0, 0, 0, 0, 0, 1, 0
+                      , 0, 0, 1, 0, 0, 0, 0, 0, 1, 0
+                      , 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+                      , 0, 1, 1, 2, 1, 1, 1, 2, 1, 1
+                      , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                      , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                      , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                      , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                      , 2, 2, 2, 1, 1, 1, 0, 0, 0, 0
+                      ]
+          countDoubled input @?= 5
+        ]
+      , testGroup "createMap"
+        [ testCase "returns an empty map vector" $ do
+            V.length createMap @?= mapDimension * mapDimension 
+            V.all (==0) createMap @? "initialized to zeros"
+        ]
+      , testGroup "mapSegments"
+        [ testCase "marks starting position on the map" $ do
+          let segment  = ((2, 1), (2, 4))
+              m        = mapSegments createMap [segment]
+          m V.! 1002 @?= 1
+        , testCase "increments values for overlapping segments" $ do
+          let m0 = createMap
+              m1 = mapSegments m0 [((2, 1), (2, 4)), ((2, 4), (7, 4))]
+          m1 V.! 4002 @?= 2
+        ]
+      , testGroup "expandSegment"
+        [ testCase "includes starting position" $ do
+          let segment  = ((2, 1), (2, 4))
+          (2, 1) `elem` expandSegment segment @? "includes starting point"
+        , testCase "includes ending position" $ do
+          let segment = ((2, 1), (2, 4))
+          (2, 4) `elem` expandSegment segment @? "includes ending point"
+        , testCase "when start and end are the same, output one item" $ do
+          let segment = ((2, 1), (2, 1))
+          length (expandSegment segment) @?= 1
+        , testCase "includes middle positions going top-to-bottom" $ do
+          let segment = ((2, 1), (2, 4))
+          (2, 2) `elem` expandSegment segment @? "includes middle point"
+          (2, 3) `elem` expandSegment segment @? "includes middle point"
+        , testCase "includes middle positions going bottom-to-top" $ do
+          let segment = ((2, 4), (2, 1))
+          (2, 2) `elem` expandSegment segment @? "includes middle point"
+          (2, 3) `elem` expandSegment segment @? "includes middle point"
+        , testCase "includes middle positions going left-to-right" $ do
+          let segment = ((1, 2), (4, 2))
+          (2, 2) `elem` expandSegment segment @? "includes middle point"
+          (3, 2) `elem` expandSegment segment @? "includes middle point"
+        , testCase "includes middle positions going right-to-left" $ do
+          let segment = ((4, 2), (1, 2))
+          (2, 2) `elem` expandSegment segment @? "includes middle point"
+          (3, 2) `elem` expandSegment segment @? "includes middle point"
+        , testCase "returns an empty list if the points are on a diagonal" $ do
+          let segment = ((1, 7), (4, 20))
+          length (expandSegment segment) @?= 0
+        ]
+      , testGroup "pointIndex"
+        [ testCase "maps first row to the column" $ do
+          pointIndex (4, 0) @?= 4
+        , testCase "maps the first column to a multiple of the dimensions" $ do
+          pointIndex (0, 7) @?= 7 * mapDimension
+        , testCase "adds the row to the multiple of the dimensions" $ do
+          pointIndex (3, 4) @?= 4 * mapDimension + 3
         ]
       ]
     , testGroup "part 2"
