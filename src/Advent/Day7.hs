a9981cb17f87900e1805a3a6861fc989fd78fbfb
Eric Rochester <erochest@gmail.com>
2021-12-15 06:08:26 -0500

    day 7, part 2, wip

--- src/Advent/Day7.hs
+++ src/Advent/Day7.hs
@@ -9,18 +9,48 @@
 module Advent.Day7
   ( partOne
   , partTwo
+  , Crab(..)
+  , findDistances
+  , findFuelCosts
+  , fuelCost
   ) where
 
+import Control.Arrow ((&&&))
 import Debug.Trace
 
 import Advent
 import qualified Data.ByteString.Lazy.Char8 as C
+import Data.Foldable
 
-partOne :: () -> Int
-partOne = undefined
+partOne :: [Crab] -> Int
+partOne crabs = minimum $ map snd $ findDistances crabMin crabMax crabs
+  where
+    crabMin = getPosition $ minimum crabs
+    crabMax = getPosition $ maximum crabs
 
-partTwo :: () -> Int
-partTwo = undefined
+partTwo :: [Crab] -> Int
+partTwo crabs = minimum $ map snd $ findFuelCosts crabMin crabMax crabs
+  where
+    crabMin = getPosition $ minimum crabs
+    crabMax = getPosition $ maximum crabs
 
-instance Parseable () where
-  parse = undefined
+newtype Crab = Crab { getPosition :: Int }
+  deriving (Eq, Show, Ord)
+
+instance Parseable [Crab] where
+  parse = traverse (fmap (Crab . fst) . C.readInt) . C.split ','
+
+findDistances :: Int -> Int -> [Crab] -> [(Int, Int)]
+findDistances a b crabs = map (id &&& (sum . findDistancesFrom crabs)) [a..b+1]
+  where
+    findDistancesFrom :: [Crab] -> Int -> [Int]
+    findDistancesFrom crabs x = map (abs . (x -) . getPosition) crabs
+
+findFuelCosts :: Int -> Int -> [Crab] -> [(Int, Int)]
+findFuelCosts a b crabs = map (id &&& sum . findFuelCostsFrom crabs) [a..b]
+  where
+    findFuelCostsFrom :: [Crab] -> Int -> [Int]
+    findFuelCostsFrom crabs x = map (fuelCost x . getPosition) crabs
+
+fuelCost :: Int -> Int -> Int 
+fuelCost a b = sum $ [0 .. abs (a - b)]
